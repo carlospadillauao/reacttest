f7d6e5abdb5a360e44eab4699ddb674a50bd0374
Carlos Padilla <padillabusines@gmail.com>
2021-05-28 17:56:56 -0500

    update react base aplication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import {BrowserRouter as Router , Route, Switch} from 'react-router-dom'
+
 
 function App() {
+  fetch("https://pokeapi.co/api/v2/pokemon/pikachu/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+        },
+ 
+        (error) => {
+          console.log(error)
+        }
+      )
   return (
-    <div>
-      hola mundo
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/hola">
+          <div>
+            mundo
+          </div>
+        </Route>
+        <Route path="">
+          <div>
+            hola mundo
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
